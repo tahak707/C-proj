6a8e568c84c4014d1b5bc85a8c1d640c7b6b6b87
Taha Khan <145873653+tahak707@users.noreply.github.com>
2023-10-28 21:59:50 +0500

    Q5.c

--- LAB1/Q5.c
+++ LAB1/Q5.c
@@ -3,25 +3,16 @@
 
 
 int fibonacci(int n) {
-    if (n <= 1) {
-        return n;
-    }
+    if (n <= 0) return 0;
+    if (n == 1) return 1;
     return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
 // Function to print the Fibonacci series up to the nth number
-void printFibonacciSeries(int n) {
-    if (n < 0) {
-        printf("Invalid input. Please enter a non-negative integer.\n");
-        return;
-    }
-
-    printf("Fibonacci Series up to the %dth number: ", n);
-    for (int i = 0; i < n; i++) {
-        printf("%d ", fibonacci(i));
-    }
-    printf("\n");
-}
+void printFib(int n, current) {
+    if (current<=n){printf("%d",fibonacci(current));
+        printFib(n, current+1);
+}}
 
 int main() {
     int n;
@@ -29,8 +20,14 @@ int main() {
     // Input the value of n
     printf("Enter the value of n: ");
     scanf("%d", &n);
+    if (n < 0) {
+        printf("Invalid input. Please enter a non-negative integer.\n");
+        return;
+    }
 
-    printFibonacciSeries(n);
+else{     printf("Fibonacci Series up to the %dth number: ", n);
+    printFib(n,0);
+     printf("/n")
 
     return 0;
 }
